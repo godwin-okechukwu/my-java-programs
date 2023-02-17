fe93c4a9c92cfbe4223b2bc65dbe7d12b8689a00
godwin-okechukwu <godwinolewunne@gmail.com>
2023-02-17 02:24:17 +0100

    This program takes three names and one conjunction asarguments at the terminal

---
+++ 1-elements-of-programming/0-ch_tasks/UseThreeArguments.java
@@ -0,0 +1,15 @@
+public class UseThreeArguments
+{
+  public static void main(String[] args)
+  {
+    System.out.print("Hi, ");
+    System.out.print(args[0]);
+    System.out.print(" ");
+    System.out.print(args[1]);
+    System.out.print(" ");
+    System.out.print(args[2]);
+    System.out.print(" ");
+    System.out.print(args[3]);
+    System.out.print(". How are you?");
+  }
+}
