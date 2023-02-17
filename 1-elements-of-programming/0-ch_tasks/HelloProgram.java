3dee8ceb8ab2eff919d02dd9cc8b68f27200c50b
godwin-okechukwu <godwinolewunne@gmail.com>
2023-02-17 01:32:12 +0100

    second commit, having added progress directories and writing a welcome message to programmers

---
+++ 1-elements-of-programming/0-ch_tasks/HelloProgram.java
@@ -0,0 +1,8 @@
+public class HelloProgram
+{
+  public static void main(String[] args)
+  {
+    //Prints "Welcome to Computer Programming!" to screen
+    System.out.println("Welcome to Computer Programming!");
+  }
+}
