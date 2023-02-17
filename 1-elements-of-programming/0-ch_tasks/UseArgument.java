39cb4e491f91ade5d1c7a1e9e35afa0a6e5a5850
godwin-okechukwu <godwinolewunne@gmail.com>
2023-02-17 01:44:34 +0100

    wrote a program that takes a name as argument at the terminal before execution

---
+++ 1-elements-of-programming/0-ch_tasks/UseArgument.java
@@ -0,0 +1,10 @@
+public class UseArgument
+{
+  //This program takes a name as argument on the terminal during compilation
+  public static void main(String[] args)
+  {
+    System.out.print("Hi, ");
+    System.out.print(args[0]);
+    System.out.println(". How are you?");
+  }
+}
